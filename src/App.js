ed349510fc556b1ae6e0795aec48e03f7c6e6b6e
Akash <akashhbti123@gmail.com>
2020-08-23 10:53:31 +0530

    changed main page text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there akash prakash.
+          Hello there from feature branch.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          feature branch.
         </a>
       </header>
     </div>
